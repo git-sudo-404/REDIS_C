845773b014de71e3e084bbade0b84c47cb765573
git-coder <ID+username@users.noreply.github.com>
2025-08-27 01:00:20 +0530

    server client ready , need to work on utils

--- practise/client.c
+++ practise/client.c
@@ -1,48 +1,33 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 #include<unistd.h>
 #include<netinet/in.h>
-#include<sys/socket.h>
-#include<stdlib.h>
-
-
 
 int main(){
-
-  int fd = socket(AF_INET,SOCK_STREAM,0) ;
+  
+  int fd = socket(AF_INET,SOCK_STREAM,0);
 
   struct sockaddr_in server_addr = {} ;
-
   server_addr.sin_family = AF_INET ;
+  server_addr.sin_addr.s_addr = htonl(0) ;
   server_addr.sin_port = htons(1234) ;
-  server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK) ;
 
-
-  int rv = connect(fd,(const struct sockaddr*) &server_addr,sizeof(server_addr)) ;
+  
+  int rv = connect(fd,(struct sockaddr*)&server_addr,sizeof(server_addr)) ;
 
   if(rv){
-    perror("Client : connect()") ;
+    perror("client : connect()") ;
     exit(1) ;
   }
-
-  char rbuf[1024] = {} ;
-  char wbuf[1024] = {} ;
-
-
-  while(1){
     
-    printf("You : ");
-    scanf("%s",wbuf);
+  printf("\nConnected with Server\n");
 
-    write(fd,wbuf,sizeof(wbuf));
+  chat_with_server(fd) ;
+  
 
-    read(fd,rbuf,sizeof(rbuf));
-    printf("\nServer : %s\n",rbuf);
-  
-  } 
-  
-  printf("Connection Closing...");
+
   close(fd) ;
-    
+
 
 }
--- practise/server.c
+++ practise/server.c
@@ -1,50 +1,26 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
 #include<netinet/in.h>
-#include<sys/socket.h>
-#include<stdlib.h>
+#include<utils.h>
 
-
-void process_connection(int client_fd){
-
-  char rbuf[1024] = {};
-  char wbuf[1024] = {};
-
-  while(1){
-    
-    
-    read(client_fd,rbuf,sizeof(rbuf));
-    printf("\nClient : %s\n",rbuf);
-
-    printf("You : ");
-    scanf("%s",wbuf);
-
-    write(client_fd,wbuf,sizeof(wbuf));
-
-    if(strcmp("BYE",wbuf)==0){  
-      return ;
-    }
-
-  }
-
-}
 
 
 int main(){
 
-  int fd = socket(AF_INET,SOCK_STREAM,0) ;
+  int fd = socket(AF_INET,SOCK_STREAM,0);
 
-  int re_use_address = 1;
-  setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&re_use_address,sizeof(re_use_address)) ;
+  int re_use_addr = 1;
+  setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&re_use_addr,sizeof(re_use_addr));
 
-  struct sockaddr_in server_addr = {} ;
+  struct sockaddr_in server_addr = {};
 
   server_addr.sin_family = AF_INET ;
   server_addr.sin_port = htons(1234) ;
-  server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK) ;
+  server_addr.sin_addr.s_addr = htonl(0) ;
 
-  int rv = bind(fd,(const struct sockaddr*) &server_addr ,sizeof(server_addr)) ;
+  int rv = bind(fd,(const struct sockaddr*) &server_addr,sizeof(server_addr)) ;
 
   if(rv){
     perror("bind()");
@@ -53,30 +29,27 @@ int main(){
 
   rv = listen(fd,SOMAXCONN) ;
 
-  if(rv){
+  if(rv<0){
     perror("listen()");
     exit(1);
   }
-  
-  printf("Server listening on PORT : %d\n",ntohs(server_addr.sin_port)) ;
+
+  printf("\nServer Running on PORT : %d\n",server_addr.sin_port) ;
 
   while(1){
   
     struct sockaddr_in client_addr = {} ;
+
     socklen_t client_addr_len = sizeof(client_addr) ;
-    int client_fd = accept(fd,(struct sockaddr*)&client_addr , &client_addr_len ) ;
 
-    if(client_fd<0){
-      perror("client_accpet()");
-      exit(1);
-    }
+    int client_fd = accept(fd,(struct sockaddr*) &client_addr ,&client_addr_len) ;
+  
+    chat_with_client(client_fd) ;
 
-    process_connection(client_fd) ;
-
-    close(client_fd);
+    close(client_fd) ;
 
   }
 
-  close(fd);
+  close(fd) ;
 
 }
