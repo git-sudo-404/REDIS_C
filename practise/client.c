751df96f4b945c437346d17dca4c20e49ca5edc6
git-coder <ID+username@users.noreply.github.com>
2025-08-26 13:41:46 +0530

    chat_app done

--- practise/client.c
+++ practise/client.c
@@ -1,39 +1,48 @@
 #include<stdio.h>
-#include<unistd.h>
 #include<string.h>
+#include<unistd.h>
 #include<netinet/in.h>
-#include<sys/types.h>
 #include<sys/socket.h>
+#include<stdlib.h>
+
 
 
 int main(){
 
-  int fd = socket(AF_INET,SOCK_STREAM,0);
+  int fd = socket(AF_INET,SOCK_STREAM,0) ;
 
-  struct sockaddr_in server_addr = {};
+  struct sockaddr_in server_addr = {} ;
 
   server_addr.sin_family = AF_INET ;
   server_addr.sin_port = htons(1234) ;
-  server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+  server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK) ;
 
-  int rv = connect(fd,(const struct sockaddr *)&server_addr,sizeof(server_addr)) ; 
 
-  if(rv){perror("client : connect()");}
+  int rv = connect(fd,(const struct sockaddr*) &server_addr,sizeof(server_addr)) ;
 
-  char msg[] = "Hi There from Client" ;
-
-  write(fd,msg,strlen(msg));
-
-  char rbuf[64]={};
-
-  ssize_t n = read(fd,rbuf,sizeof(rbuf) - 1);
-
-  if(n<0){
-    perror("client : read()");
+  if(rv){
+    perror("Client : connect()") ;
+    exit(1) ;
   }
 
-  printf("\nMessage from Server : %s\n",rbuf);
-  
-  close(fd);
+  char rbuf[1024] = {} ;
+  char wbuf[1024] = {} ;
 
-} 
+
+  while(1){
+    
+    printf("You : ");
+    scanf("%s",wbuf);
+
+    write(fd,wbuf,sizeof(wbuf));
+
+    read(fd,rbuf,sizeof(rbuf));
+    printf("\nServer : %s\n",rbuf);
+  
+  } 
+  
+  printf("Connection Closing...");
+  close(fd) ;
+    
+
+}
--- practise/server.c
+++ practise/server.c
@@ -1,108 +1,82 @@
-#include<sys/socket.h>  // socket 
-#include<stdio.h>       // read , write 
-#include<unistd.h>      // close 
-#include<string.h>      // strlen 
-#include<netinet/in.h>  // struct sockaddr_in , htons ,... 
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<stdlib.h>
 
-static void do_something(int connfd){
 
-  char buffer[1024] = {};
+void process_connection(int client_fd){
 
-  ssize_t n = read(connfd,buffer,sizeof(buffer) - 1);
-  
-  if(n<0){
-    perror("error : read()");
-    return;
+  char rbuf[1024] = {};
+  char wbuf[1024] = {};
+
+  while(1){
+    
+    
+    read(client_fd,rbuf,sizeof(rbuf));
+    printf("\nClient : %s\n",rbuf);
+
+    printf("You : ");
+    scanf("%s",wbuf);
+
+    write(client_fd,wbuf,sizeof(wbuf));
+
+    if(strcmp("BYE",wbuf)==0){  
+      return ;
+    }
+
   }
-
-  printf("\n\t Simon says : %s\n",buffer) ; 
-
-  char wbuf[] = "King of the World" ; 
-
-  write(connfd,wbuf,strlen(wbuf));
-
-
-/* ssize_t read(int fd, void *buf, size_t len); */
-/* ssize_t recv(int fd, void *buf, size_t len, int flags);         // read */
-/* ssize_t write(int fd, const void *buf, size_t len); */
-/* ssize_t send(int fd, const void *buf, size_t len, int flags);   // write */
-// send and recv has an extra optional flags 
 
 }
 
 
-
 int main(){
+
+  int fd = socket(AF_INET,SOCK_STREAM,0) ;
+
+  int re_use_address = 1;
+  setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&re_use_address,sizeof(re_use_address)) ;
+
+  struct sockaddr_in server_addr = {} ;
+
+  server_addr.sin_family = AF_INET ;
+  server_addr.sin_port = htons(1234) ;
+  server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK) ;
+
+  int rv = bind(fd,(const struct sockaddr*) &server_addr ,sizeof(server_addr)) ;
+
+  if(rv){
+    perror("bind()");
+    exit(1);
+  }
+
+  rv = listen(fd,SOMAXCONN) ;
+
+  if(rv){
+    perror("listen()");
+    exit(1);
+  }
   
-  // Step 1 : Create a file descriptor for a new socket
-  int fd = socket(AF_INET,SOCK_STREAM,0);
-
-  // Step 2 : Configure the socket use setsockopt to reuse the same IP and PORT , 
-  //              --> When the server restarts (Connection is closed and opened again)
-  //                  the TCP Connection would not have been closed immediatly , it would be in TIME_WAIT state , (see SO_REUSEADDR)
-  //                  ans it'll throw some error saying the PORT is already in use, by using SO_REUSEADDR we tell the OS , it's ok
-  //                  and reuse the same address 
-
-  int reuse_address_val = 1; // 1  - indicates the SO_REUSEADDR is ON 
-                            // SO_REUSEADDR accepts 1 or 0 
-
-  setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&reuse_address_val,sizeof(reuse_address_val));
-
-  // Step 3 : Bind the socket to an Address 
-  //            --> struct sockadrr_in is specially designed struct to hold all the neccessary info for an IPv4 network address 
-
-  struct sockaddr_in bind_address = {};   // {} -- initializes all fields to 0 
-
-  //  struct sockaddr_in {
-  //      uint16_t       sin_family; // AF_INET
-  //      uint16_t       sin_port;   // port in big-endian
-  //      struct in_addr sin_addr;   // IPv4
-  //  };
-  //  struct in_addr {
-  //      uint32_t       s_addr;     // IPv4 in big-endian
-  //  };
-
-
-   
-  // Setting the address 
-
-  bind_address.sin_family = AF_INET ; // AF_INET - IPv4 , AF_INET6 - IPv6 
-  bind_address.sin_port = htons(1234) ; // bind the PORT number, htons - host to network byte order short 
-  bind_address.sin_addr.s_addr = htonl(0) ; // 0 -- wildcard address for 0.0.0.0   
-  //                                            --> set to wildcard address (0.0.0.0) , which enables it to listen to all the IP's 
-  //                                              (i.e) a client can connect to the server from any IP 
-
-  // Perform the binding 
-
-  int rv = bind(fd,(const struct sockaddr*)&bind_address,sizeof(bind_address)) ;  // 0 - on success , non - zero value - on failure 
-  
-  if(rv){perror("bind()");}
-
-  // Step 4 : Listen -- here is where the actual socket is created 
-
-  rv = listen(fd,SOMAXCONN); // the 2nd argument is the size of the queue  -- SOMAXCONN is 4096 in Linux 
-
-  if(rv){perror("listen()");}
-
-  printf("\nServer Listeing on PORT : %d\n",ntohs(bind_address.sin_port));
+  printf("Server listening on PORT : %d\n",ntohs(server_addr.sin_port)) ;
 
   while(1){
   
     struct sockaddr_in client_addr = {} ;
-    socklen_t addrlen = sizeof(client_addr) ; 
-    int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen) ;      // connfd has the file descriptor of the client connection 
-    
+    socklen_t client_addr_len = sizeof(client_addr) ;
+    int client_fd = accept(fd,(struct sockaddr*)&client_addr , &client_addr_len ) ;
 
-    if(connfd<0){
-      perror("accept()");
+    if(client_fd<0){
+      perror("client_accpet()");
+      exit(1);
     }
 
-    do_something(connfd);
+    process_connection(client_fd) ;
 
-    close(connfd) ;
+    close(client_fd);
 
+  }
 
-  } 
-  
+  close(fd);
 
 }
