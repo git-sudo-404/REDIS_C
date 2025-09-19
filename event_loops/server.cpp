e4c7a1f166e54fbc408cc23a074d0e79cc7e3445
git-coder <ID+username@users.noreply.github.com>
2025-09-20 01:38:47 +0530

    implementing own logic for event loops

--- event_loops/server.cpp
+++ event_loops/server.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 #define PORT 8080
 
-// NOTE: No need to redefine it again since it is already imported using the poll.h .
+// No need to redefine it again since it is already imported using the poll.h .
 // struct pollfd{  // This is the struct that should be passed to the poll() system call.
 //
 //   int fd;
@@ -62,7 +62,44 @@ int main(){
 
   while(1){
     
-    int number_of_events = poll(pfds,pfds.size(),-1) ;
+    int number_of_events = poll(pfds,pfds.size(),-1) ; 
+
+    // NOTE: Timout - 0 : listen on all the fds and return immediatly .
+    // Having a Timout of 0 does work, coz if there's no event it returns immediatly and 
+    // Then once again poll() is called coz of the while loop.
+    // This causes the CPU to be executing always even when there's nothing really happening.
+    //
+    // So , instead it's better to use -1 , which keeps on listening untill there's atleast one 
+    // event that triggers it happens. Here , a new client connection being made.
+
+    if(number_of_events<0){
+      perror("Error : poll()");
+      break;
+    }
+
+    // Check the listening socket for new client connection.
+    if(pfds[0].revents & POLLIN){
+      struct sockaddr_in client_addr = {};
+      socklen_t client_addr_len = sizeof(client_addr) ;
+      
+      int client_fd = accept(fd,(struct sockaddr*)&client_addr,&client_addr_len) ;
+      if(client_fd<0){
+        perror("Error : accept()");
+      } else {
+        cout<<"New Connection made with Client : "<<client_fd<<endl;
+        
+        struct pollfd client_pfd = {};
+        client_pfd.fd = client_fd ;
+        client_pfd.events = POLLIN ; // Monitor for incoming data .
+        client_pfd.revents = 0 ;
+
+        pfds.pb(client_pfd) ;
+      }
+
+    }
+
+    // Loop through all the client fds and check for data.
+    
 
   }
 
